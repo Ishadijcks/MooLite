4f8d65be3ae75bbb3515bfa866babd57620933ea
Isha Dijcks <ishadijcks@gmail.com>
2023-05-09 19:50:27 +0200

    chore(MooLite): Prettier

--- src/MooLite/core/server/messages/LootOpened.ts
+++ src/MooLite/core/server/messages/LootOpened.ts
@@ -7,13 +7,13 @@ import { CharacterItem } from "src/MooLite/core/inventory/CharacterItem";
 export interface LootOpenedMessage extends ServerMessage {
     type: ServerMessageType.LootOpened;
     openedItem: CharacterItem;
-    gainedItems: CharacterItem[]
+    gainedItems: CharacterItem[];
 }
 
 export class LootOpenedParser extends MessageParser {
     type = ServerMessageType.LootOpened;
 
     apply(message: LootOpenedMessage, game: Game): void {
-        game.lootBoxes.lootBoxOpened(message.openedItem, message.gainedItems)
+        game.lootBoxes.lootBoxOpened(message.openedItem, message.gainedItems);
     }
 }
