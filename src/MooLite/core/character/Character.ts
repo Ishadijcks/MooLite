6dae349d30253e35887d7b6c41626e5c39c22565
Ronald Jimmy Headrick III <jimmy@appsmith.com>
2023-06-17 22:05:23 -0400

    Run linter

--- src/MooLite/core/character/Character.ts
+++ src/MooLite/core/character/Character.ts
@@ -1,13 +1,13 @@
 export interface Character {
-  id: number;
-  userID: number;
-  name: string;
-  previousName: string;
-  chatIconHrid: string;
-  isOnline: boolean;
-  lastOfflineTime: string;
-  inactiveTime: string;
-  isDeleted: boolean;
-  createdAt: string;
-  updatedAt: string;
+    id: number;
+    userID: number;
+    name: string;
+    previousName: string;
+    chatIconHrid: string;
+    isOnline: boolean;
+    lastOfflineTime: string;
+    inactiveTime: string;
+    isDeleted: boolean;
+    createdAt: string;
+    updatedAt: string;
 }
