520c9056cd9ee6acbea4d6ea8c8fb0df48286081
Ronald Jimmy Headrick III <jimmy@appsmith.com>
2023-06-18 23:12:24 -0400

    run linter

--- src/MooLite/core/plugins/MooLiteTab.ts
+++ src/MooLite/core/plugins/MooLiteTab.ts
@@ -13,5 +13,18 @@ export interface MooLiteTab {
     component: Component;
 
     /** Expects a tailwind width class. */
-    width?: "w-32" | "w-36" | "w-40" | "w-44" | "w-48" | "w-52" | "w-56" | "w-60" | "w-64" | "w-72" | "w-80" | "w-96" | `w-[${string}${"em" | "rem" | "px" | "%"}]`;
+    width?:
+        | "w-32"
+        | "w-36"
+        | "w-40"
+        | "w-44"
+        | "w-48"
+        | "w-52"
+        | "w-56"
+        | "w-60"
+        | "w-64"
+        | "w-72"
+        | "w-80"
+        | "w-96"
+        | `w-[${string}${"em" | "rem" | "px" | "%"}]`;
 }
