2e924302f6f0601e3b713f563617941bed76c457
Ronald Jimmy Headrick III <jimmy@appsmith.com>
2023-06-22 15:50:48 -0400

    add Isha to contributor list

--- src/MooLite/plugins/WhisperManager/DevConstants.ts
+++ src/MooLite/plugins/WhisperManager/DevConstants.ts
@@ -1,7 +1,7 @@
 import { ChatChannelTypeHrid } from "src/MooLite/core/chat/ChatChannelTypeHrid";
 import { ConversationMessage } from "src/MooLite/plugins/WhisperManager/ConversationMessage";
 
-export const contributorNames: string[] = ["LagunaE"];
+export const contributorNames: string[] = ["LagunaE", "Sleutelkind"];
 export const betaTesterNames: string[] = ["JBQ", "knighterd", "Thessaloniki"];
 export const helperNames: string[] = ["Champ", "ML", "Maxion", "hoador"];
 
