7c59f1d9f7fa13811e758c69a4d6bf9678d597da
Ronald Jimmy Headrick III <jimmy@appsmith.com>
2023-07-09 02:05:17 -0400

    polish color samples

--- src/components/histoire-controls/Color/ColorSample.vue
+++ src/components/histoire-controls/Color/ColorSample.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import CopyIcon from "src/components/icons/CopyIcon.vue";
-import { useClipboard } from "@vueuse/core";
-import { VTooltip as vTooltip } from "floating-vue";
 import { computed, ref } from "vue";
 import ColorSampleType from "src/components/histoire-controls/Color/ColorSampleType";
 import CopyableText from "src/components/histoire-controls/Text/CopyableText.vue";
@@ -10,12 +7,10 @@ export interface ColorSampleProps {
     colorClass: string;
     value: string;
     type?: ColorSampleType;
+    sampleText?: string;
 }
 
 const props = defineProps<ColorSampleProps>();
-
-const { copy: copyClass, copied: copiedClass } = useClipboard();
-const { copy: copyValue, copied: copiedValue } = useClipboard();
 
 const showCopyButtons = ref(false);
 
@@ -28,34 +23,21 @@ const sampleClass = computed(() => {
         case ColorSampleType.Background:
             return `rounded-mwi-default bg-${props.colorClass}`;
         case ColorSampleType.Border:
-            return `rounded-mwi-default border border-4 border-${props.colorClass}`;
+            return `rounded-mwi-default border-2 border-${props.colorClass}`;
         case ColorSampleType.Text:
             return `rounded-mwi-default text-${props.colorClass} text-6xl`;
-    }
-});
-
-const displayClass = computed(() => {
-    switch (sampleType.value) {
-        case ColorSampleType.Sample:
-            return props.colorClass;
-        case ColorSampleType.Background:
-            return `bg-${props.colorClass}`;
-        case ColorSampleType.Border:
-            return `border-${props.colorClass}`;
-        case ColorSampleType.Text:
-            return `text-${props.colorClass}`;
     }
 });
 </script>
 
 <template>
     <div
-        class="flex flex-col w-64 text-dark-mode border-2.5 rounded-mwi-default"
+        class="flex flex-col w-64 text-dark-mode rounded-mwi-default"
         @mouseenter="showCopyButtons = true"
         @mouseleave="showCopyButtons = false"
     >
         <div class="h-16" :class="sampleClass">
-            {{ props.type === ColorSampleType.Text ? "Aa 0-9" : null }}
+            <span v-if="props.type === ColorSampleType.Text">{{ props.sampleText || "Aa 0-9" }}</span>
         </div>
         <div class="flex flex-col px-1.5 py-1">
             <div class="flex">
