f3baa8461c951aa03c59edc198a8b51fecf89a75
Isha Dijcks <isha@q42.nl>
2023-07-17 05:29:42 +0200

    fix(LeaderboardPlugin): Update leaderboard to handle new structure introduced for tasks
    The current implementation does not scale to various types of leaderboards, but it works for now.
    If another type is introduced it would be better to refactor it such that we can predefine a set of categories that will be parsed and rendered correctly automatically

--- src/components/atoms/ItemIcon.vue
+++ src/components/atoms/ItemIcon.vue
@@ -4,18 +4,19 @@ import { ItemHrid } from "src/MooLite/core/inventory/ItemHrid";
 
 const props = defineProps<{
     item: ItemHrid;
+    size: string;
 }>();
 
 const imgUrl = computed(() => {
     const split = (props.item as unknown as string).split("/");
     const itemPostfix = split[split.length - 1];
     // TODO(@Isha): Research how these CDN resource urls are generated
-    return `/static/media/items_sprite.20cec8ed.svg#${itemPostfix}`;
+    return `/static/media/items_sprite.951ef1ec.svg#${itemPostfix}`;
 });
 </script>
 
 <template>
-    <svg class="w-8 h-8">
+    <svg :class="'w-' + (size ?? 8) + ' h-' + (size ?? 8)">
         <use :href="imgUrl"></use>
     </svg>
 </template>
